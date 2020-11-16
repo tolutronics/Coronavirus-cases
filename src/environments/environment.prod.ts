53f77c82efe09010f24ffc14f5f3e8fe8f45b1fe
tolutronics <toluxy100@gmail.com>
2020-11-16 10:41:19 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC_BWC4ttJQMXcgLLHFnHL46L4Q5DNIBvU",
-    authDomain: "coronavirus-pan.firebaseapp.com",
-    databaseURL: "https://coronavirus-pan.firebaseio.com",
-    projectId: "coronavirus-pan",
-    storageBucket: "coronavirus-pan.appspot.com",
-    messagingSenderId: "788932357380",
-    appId: "1:788932357380:web:3a234d3e6ab273ee147451",
-    measurementId: "G-D0D54C6ZNV"
+   
   }
 
 }
